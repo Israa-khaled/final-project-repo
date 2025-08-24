bc9c5d5eef13da78a2adf5dc19740cef1a689991
Israa-khaled <122534323+Israa-khaled@users.noreply.github.com>
2025-08-24 15:35:34 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# simple-interest.sh : calculate simple interest
+# Usage: ./simple-interest.sh principal rate time
 
+P=$1
+R=$2
+T=$3
+
+SI=$(( (P * R * T) / 100 ))
+
+echo "Simple Interest = $SI"
